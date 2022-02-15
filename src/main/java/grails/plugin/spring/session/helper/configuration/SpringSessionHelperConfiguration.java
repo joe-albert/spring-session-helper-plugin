2b56e21c28ea3cc7adcb54caee6390fa7c5bfcf9
Simon Bear <simon.bear@csiro.au>
2022-02-15 16:18:12 +1100

    Allow Spring Session helper beans to be disabled at runtime

--- src/main/java/grails/plugin/spring/session/helper/configuration/SpringSessionHelperConfiguration.java
+++ src/main/java/grails/plugin/spring/session/helper/configuration/SpringSessionHelperConfiguration.java
@@ -3,12 +3,14 @@ package grails.plugin.spring.session.helper.configuration;
 import grails.plugin.spring.session.helper.filter.MutableSessionStateSynchronizingFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.SessionRepository;
 
 @Configuration
+@ConditionalOnProperty(prefix="spring.session", name="enabled", havingValue = "true")
 @ConditionalOnBean(SessionRepository.class)
 @EnableConfigurationProperties(SpringSessionHelperProperties.class)
 public class SpringSessionHelperConfiguration {
